6d1bc2b0f09445e376486449ba421613079105c5
Dunge0N3 <estebancarignani99@gmail.com>
2022-03-21 19:58:31 -0300

    Inicio TP1.1, TP1.2, TP2.1

---
+++ AplicacionDeConsola/AplicacionDeConsola/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ Lab02/Lab02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var valor1 = "Este es el valor 1.";
+            var valor2 = 5;
+
+            var valor3 = valor2.ToString();
+
+            Console.WriteLine(valor1);
+            Console.WriteLine(valor2);
+            Console.WriteLine(valor3);
+
+            Console.ReadKey();
+        }
+    }
+}
